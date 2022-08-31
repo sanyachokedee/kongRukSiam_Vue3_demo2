9c56fa6ad711dc2f74b523732edbd0b9e98f7fb5
Nutthakit Anansuksakul <ing118199@gmail.com>
2022-08-31 12:57:03 +0700

    two-ways input radio

--- src/components/FormComponent.vue
+++ src/components/FormComponent.vue
@@ -10,6 +10,17 @@
             <input type="number" v-model="employee.salary">
             <p>{{employee.salary}}</p>
         </div>
+        <div class="form-control">
+            <h2>เพศ</h2>
+            <div>
+                <input type="radio" value="ชาย" v-model="employee.gender" />
+                <label for="gender-name">ชาย</label>
+            </div>
+            <div>
+                <input type="radio" value="หญิง" v-model="employee.gender" />
+                <label for="gender-name">หญิง</label>
+            </div>
+        </div>
         <div>
             <button>บันทึกข้อมูล</button>
         </div>
@@ -24,7 +35,8 @@ export default {
         return {
             employee: {
                 name: "",
-                salary:25000
+                salary: 25000,
+                gender: ""
             }
         }
     }
@@ -58,5 +70,17 @@ export default {
         cursor:pointer;
         padding: 0.75rem 2rem;
         border-radius: 5px;
+    }
+    input[type="radio"]{
+        display: inline-block;
+        width:auto;
+        margin-right: 1rem;
+    }
+    input[type="radio"]+label{
+        font-weight: normal;
+    }
+    h2{
+        font-size: 1rem;
+        margin:0.5rem 0;
     }
 </style>
